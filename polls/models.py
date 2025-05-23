49ae0c905ec9aee5536c4b9d87c9a7f083d28c07
Enrico Meluziis <enricomlz@hotmail.it>
2025-05-23 17:02:27 +0200

    adds a custom method to Question model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.utils import timezone
 
@@ -8,6 +9,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date_published")
